d9cf821e56579819093f1f640a030935ac42bb8a
travisrase <travisrase@dhcp-206-225.student-wlan.bowdoin.edu>
2019-11-07 14:36:09 -0500

    up to date

--- ACO.py
+++ ACO.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 class ACO:
     def __init__(self,numAnts,numIter,alpha,beta,rho,epsilon,tao0,q0,problem):
         #number of ants in the colony
@@ -18,14 +19,29 @@ class ACO:
         #a list of all ant solutions
         self.ants = []
 
-    def initMatrix(self):
+    def initPhermoneMatrix(self):
         #init n x n 2d array where n = size of the problem
         self.permoneMatrix = np.zeros[(len(self.problem),(len(self.problem))]
 
     def solve(self):
-        self.initMatrix()
+        #initialize the phermone matrix
+        self.initPhermoneMatrix()
         i = 0
         while (i < self.numIter):
             for ant in range(self.numAnts):
+                #the path created by a given ant
                 path = []
-                #randomly choose starting node
+                #the nodes that have not yet been visited by the ant
+                unvisitedNodes = self.problem
+                for node in range(len(self.problem)):
+                    path,unvisitedNodes = self.getNextNode(path,unvisitedNodes)
+
+
+    def getNextNode(self,path,unvisitedNodes):
+        nextNodeIndex
+        if node == 0:
+            #randomly generate starting node index
+            startIndex = random.randrange(0,len(self.problem))
+            startNode = remove(startIndex)
+        else:
+        print()
